8a29aeb83812b95e6965c6353eaec71cbf924497
Febrian <febrian26022001@gmail.com>
2023-11-26 10:59:58 +0700

    Add Bookmark Screen

--- shared/src/commonMain/kotlin/ui/screens/bookmark/BookmarkScreen.kt
+++ shared/src/commonMain/kotlin/ui/screens/bookmark/BookmarkScreen.kt
@@ -2,24 +2,108 @@ package ui.screens.bookmark
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.toMutableStateList
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import data.bookmark.model.BookmarkRequest
+import data.bookmark.state.ListBookmarkState
 import dev.icerock.moko.mvvm.compose.getViewModel
 import dev.icerock.moko.mvvm.compose.viewModelFactory
+import kotlinx.coroutines.CoroutineScope
+import ui.components.ProgressBarLoading
 import ui.components.TopBar
+import ui.screens.post.OptionPostEvent
+import ui.screens.post.items.ItemPostBookmark
+import ui.themes.bgColor
+import utils.getUid
+import utils.showSnackBar
 
 @Composable
 fun BookmarkScreen(
+    scaffoldState: SnackbarHostState,
+    coroutineScope: CoroutineScope
 ) {
 
     val bookmarkViewModel = getViewModel(Unit, viewModelFactory { BookmarkViewModel() })
 
+    val uid = getUid()
 
-    Column(
-        modifier = Modifier.fillMaxWidth().wrapContentSize()
+    var uidState by remember { mutableStateOf("") }
+    uidState = uid
+
+    if (uidState.isNotEmpty()) {
+        bookmarkViewModel.getAllBookmark(uidState)
+    }
+
+    val event = OptionPostEvent()
+
+    Scaffold(
+        containerColor = bgColor
     ) {
-        TopBar("Bookmark")
+        Column(
+            modifier = Modifier.fillMaxWidth().wrapContentSize()
+        ) {
+            TopBar("Bookmark")
+
+            bookmarkViewModel.listBookmarkState.collectAsState().value.onSuccess {
+                when (it) {
+                    is ListBookmarkState.Loading -> {
+                        ProgressBarLoading()
+                    }
+
+                    is ListBookmarkState.Error -> {
+                        showSnackBar("Something was wrong!", coroutineScope, scaffoldState)
+                    }
+
+                    is ListBookmarkState.Success -> {
+                        val listPost by remember { mutableStateOf(it.data.data?.toMutableStateList()) }
+                        LazyColumn(modifier = Modifier.padding(bottom = 64.dp, top = 8.dp)) {
+                            items(listPost?: listOf()) { data ->
+                                if (data.Bookmarks?.isNotEmpty() == true) {
+                                    if (data.Bookmarks.filter { it.id_user == uid }.isNotEmpty())
+
+                                        ItemPostBookmark(
+                                            data,
+                                            uid,
+                                            coroutineScope,
+                                            scaffoldState,
+                                            event,
+                                            onBookmarkPost = {
+                                                listPost?.remove(data)
+                                                bookmarkViewModel.insertBookmark(
+                                                    BookmarkRequest(data.id ?: 0, uidState)
+                                                )
+
+                                                showSnackBar(
+                                                    "Cancel Add to Bookmark",
+                                                    coroutineScope,
+                                                    scaffoldState
+                                                )
+
+                                            })
+                                }
+                            }
+                        }
+                    }
+
+                    else -> {}
+                }
+            }.onFailure {
+                showSnackBar(it.message.toString(), coroutineScope, scaffoldState)
+            }
+        }
     }
 }
--- shared/src/commonMain/kotlin/ui/screens/bookmark/BookmarkViewModel.kt
+++ shared/src/commonMain/kotlin/ui/screens/bookmark/BookmarkViewModel.kt
@@ -1,8 +1,9 @@
 package ui.screens.bookmark
 
 import data.bookmark.BookmarkSdk
-import data.bookmark.state.BookmarkState
 import data.bookmark.model.BookmarkRequest
+import data.bookmark.state.BookmarkState
+import data.bookmark.state.ListBookmarkState
 import dev.icerock.moko.mvvm.viewmodel.ViewModel
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
@@ -17,27 +18,32 @@ class BookmarkViewModel : ViewModel() {
 
     private val sdk: BookmarkSdk = BookmarkSdk()
 
-    private val _bookmarkState = MutableStateFlow<BookmarkState>(BookmarkState.Empty)
-    val bookmarkState: StateFlow<BookmarkState> get() = _bookmarkState.asStateFlow()
+    private val _bookmarkState =
+        MutableStateFlow<Result<BookmarkState>>(Result.success(BookmarkState.Empty))
+    val bookmarkState: StateFlow<Result<BookmarkState>> get() = _bookmarkState.asStateFlow()
+
+    private val _listBookmarkState =
+        MutableStateFlow<Result<ListBookmarkState>>(Result.success(ListBookmarkState.Empty))
+    val listBookmarkState: StateFlow<Result<ListBookmarkState>> get() = _listBookmarkState.asStateFlow()
 
     fun insertBookmark(bookmarkRequest: BookmarkRequest) {
-        _bookmarkState.value = BookmarkState.Loading
+        _bookmarkState.value = Result.success(BookmarkState.Loading)
         CoroutineScope(Dispatchers.IO).launch {
             _bookmarkState.value = sdk.insertBookmark(bookmarkRequest)
         }
     }
 
     fun getBookmarkById(idUser: String, bookmarkRequest: BookmarkRequest) {
-        _bookmarkState.value = BookmarkState.Loading
+        _bookmarkState.value = Result.success(BookmarkState.Loading)
         CoroutineScope(Dispatchers.IO).launch {
             _bookmarkState.value = sdk.getBookmarkById(idUser, bookmarkRequest)
         }
     }
 
     fun getAllBookmark(idUser: String) {
-        _bookmarkState.value = BookmarkState.Loading
+        _listBookmarkState.value = Result.success(ListBookmarkState.Loading)
         CoroutineScope(Dispatchers.IO).launch {
-            _bookmarkState.value = sdk.getAllBookmark(idUser)
+            _listBookmarkState.value = sdk.getAllBookmark(idUser)
         }
     }
 }
